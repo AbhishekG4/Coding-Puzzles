e4c9b559629eedd5e7bc1207caf524541349e5c2
Abhishek-prepto <abhishek@ptepto.com>
2024-11-02 12:47:19 -0700

    Addressed issues mentioned for best_line.cpp file

--- best_line.cpp
+++ best_line.cpp
@@ -1,37 +1,3 @@
-// Engineer: Abhishek Gautam
-// Last Updated: 10-27-2024
-//
-// Problem: Given a two-dimensional graph with points on it, find a line which
-//          passes the most number of points. ("Best Line" from Cracking the
-//          Coding Interview)
-//
-// Approach:
-// 1) For every point i, iterate through every unvisited point j. Each pair of
-//    points
-//    defines a line, thus each slope defines a line in association with the
-//    point i.
-// 2) For every point i, keep track of the frequency of slopes of point pairs
-//    i,j using a
-//    hashmap and find the most frequent.
-// 3) Across all points i keep track of highest frequency slope found in the
-//    previous step and that particular point.
-// 4) The highest frequency slope along with its corresponding point will define
-//    the best line being asked for.
-//
-// Time Complexity = O(n^2)
-// Space Complexity = O(n)
-//
-// Other considerations:
-// - Vertical lines (slope is inf). [Taken into account]
-// - Managing floating point precision related inaccuracies. [Not taken into
-// account for simplicity]
-//
-// Format Used:
-// - points are represented as a struct with x and y.
-// - Graph represented as vector of points
-// - Lines are represented in slope + intercept form.
-//=========================================================================================
-
 #include "best_line.h"
 
 /*Additional constructors for the Line class*/
@@ -39,7 +5,7 @@ Line::Line(double s, Point point) {  // constructor for point-slope form of line
   slope = s;
   intercept = point.y - s * point.x;
   if (s == 0)
-    isHorizontal = 1;
+    isHorizontal = true;
   else
     x_intercept = -1 * intercept / s;
 }
@@ -49,14 +15,14 @@ Line::Line(bool iV, Point point) {  // constructor for vertical lines
   x_intercept = point.x;
 }
 
-SlopeInfo getFrequentSlope(vector<Point> points, int i) {
+SlopeInfo getFrequentSlope(vector<Point> points, size_t i) {
   unordered_map<double, int> slope_counts;  // maps slope -> frequency
   int vertical_count = 0;  // To keep track of frequency of vertical slopes as
                            // it can't be tracked by slope_counts
 
   // We iterate through every other point j that forms an unseen(new) pair,
   // calculate slope with i and add to hashmap
-  for (int j = i + 1; j < points.size(); j++) {
+  for (size_t j = i + 1; j < points.size(); j++) {
     if (points[j].x == points[i].x) {  // for vertical lines
       vertical_count++;
       continue;
@@ -69,7 +35,7 @@ SlopeInfo getFrequentSlope(vector<Point> points, int i) {
   // Retrieving most frequent line
   double frequent_slope = 0;
   int max_slope_count = INT_MIN;
-  bool isVertical = 0;
+  bool isVertical = false;
   for (auto s : slope_counts) {
     if (max_slope_count < s.second) {
       max_slope_count = s.second;
@@ -78,26 +44,23 @@ SlopeInfo getFrequentSlope(vector<Point> points, int i) {
   }
   if (vertical_count > max_slope_count) {  // considering vertical lines
     max_slope_count = vertical_count;
-    isVertical = 1;
+    isVertical = true;
   }
 
-  SlopeInfo info;
-  info.value = frequent_slope;
-  info.freq = max_slope_count;
-  info.isVertical = isVertical;
-  return info;
+  return {.freq = max_slope_count,
+          .value = frequent_slope,
+          .isVertical = isVertical};
 }
 
 Line getBestLine(vector<Point> points) {
-  SlopeInfo best_slope;
-  best_slope.value = 0;
-  best_slope.freq = INT_MIN;
-  best_slope.isVertical = 0;
+  SlopeInfo best_slope{.freq = INT_MIN,
+                       .value = 0,
+                       .isVertical = false};
   int best_idx = -1;
 
   // we iterate through every point and keep track of the most frequent slope
   // passing through any point
-  for (int i = 0; i < points.size(); i++) {
+  for (size_t i = 0; i < points.size(); i++) {
     SlopeInfo point_info = getFrequentSlope(points, i);
 
     if (point_info.freq > best_slope.freq) {
--- best_line.h
+++ best_line.h
@@ -1,3 +1,37 @@
+// Engineer: Abhishek Gautam
+// Last Updated: 11-1-2024
+//
+// Problem: Given a two-dimensional graph with points on it, find a line which
+//          passes the most number of points. ("Best Line" from Cracking the
+//          Coding Interview)
+//
+// Approach:
+// 1) For every point i, iterate through every unvisited point j. Each pair of
+//    points
+//    defines a line, thus each slope defines a line in association with the
+//    point i.
+// 2) For every point i, keep track of the frequency of slopes of point pairs
+//    i,j using a
+//    hashmap and find the most frequent.
+// 3) Across all points i keep track of highest frequency slope found in the
+//    previous step and that particular point.
+// 4) The highest frequency slope along with its corresponding point will define
+//    the best line being asked for.
+//
+// Time Complexity = O(n^2)
+// Space Complexity = O(n)
+//
+// Other considerations:
+// - Vertical lines (slope is inf). [Taken into account]
+// - Managing floating point precision related inaccuracies. [Not taken into
+// account for simplicity]
+//
+// Format Used:
+// - points are represented as a struct with x and y.
+// - Graph represented as vector of points
+// - Lines are represented in slope + intercept form.
+//=========================================================================================
+
 #ifndef BEST_LINE_H
 #define BEST_LINE_H
 
@@ -26,8 +60,8 @@ class Line {
   double slope;
   double intercept = 0;  // y-intercept
   double x_intercept = 0;
-  bool isVertical = 0;
-  bool isHorizontal = 0;
+  bool isVertical = false;
+  bool isHorizontal = false;
 
   Line() : slope(0), intercept(0) {}  // default constructor
   Line(double s, Point point);
