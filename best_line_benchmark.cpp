e603f7a35af2a2c23098eb08e81364036d06d7d4
Abhishek-prepto <abhishek@ptepto.com>
2024-11-16 19:29:12 -0800

    switched random generator to use random_device

--- best_line_benchmark.cpp
+++ best_line_benchmark.cpp
@@ -1,18 +1,17 @@
 #include <benchmark/benchmark.h>
 
-#include <ctime>
 #include <random>
 
 #include "best_line.h"
 
 // Function to populate a random graph of rational numbers with n points
 std::vector<Point> PopulateRandomGraph(std::vector<Point> &graph, int n) {
-  std::mt19937 generator(static_cast<unsigned int>(std::time(NULL)));
+  std::random_device rd;
   std::uniform_real_distribution<double> distribution(-2000, 2000);
 
   for (int i = 0; i < n; i++) {
-    double x = distribution(generator);
-    double y = distribution(generator);
+    double x = distribution(rd);
+    double y = distribution(rd);
     graph.push_back({x, y});
   }
 
@@ -21,12 +20,12 @@ std::vector<Point> PopulateRandomGraph(std::vector<Point> &graph, int n) {
 
 // Function to populate a random graph of integers with n points
 std::vector<Point> PopulateRandomIntGraph(std::vector<Point> &graph, int n) {
-  std::mt19937 generator(static_cast<unsigned int>(std::time(NULL)));
+  std::random_device rd;
   std::uniform_int_distribution<int> distribution(-20, 20);
 
   for (int i = 0; i < n; i++) {
-    int x = distribution(generator);
-    int y = distribution(generator);
+    int x = distribution(rd);
+    int y = distribution(rd);
     graph.push_back({(double)x, (double)y});
   }
 
